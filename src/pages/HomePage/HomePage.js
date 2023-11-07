5d4d1a0e723e46db3b16d6b94a7880a1ff1b79d5
Brett <brettgenoe@gmail.com>
2023-11-06 20:30:24 -0800

    nav bar added, with initial styling, no media queries done

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,7 +4,15 @@ const HomePage = () => {
 
     return (
         <>
-            <main className='home' ></main>
+            <main className='home' >
+                <h1 className='home__title' >Let's Connect!</h1>
+                <h2 className='home__title-search'>Where are you searching?</h2>
+                <article className='home__map'>
+
+                    <span>insert map api here</span>
+
+                </article>
+            </main>
         </>
     )
 }
