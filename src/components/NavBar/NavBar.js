141477e6adeec2fd9bafd07d99196cd03e06958b
brettgenoe <brettgenoe@gmail.com>
2024-02-26 23:29:47 +0000

    added hamburger menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,34 @@
 import { useAuth } from '../../components/AuthContext/AuthContext'
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
 import logo from "../../assets/images/logo.png";
+import menu from "../../assets/icons/menu.svg"
 import './NavBar.scss';
 
 const NavBar = () => {
     const { loggedIn, logOut } = useAuth();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const navMenuRef = useRef(null);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (navMenuRef.current && !navMenuRef.current.contains(event.target)) {
+                setIsMenuOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [navMenuRef]);
+
+    const handleLinkClick = () => {
+        setIsMenuOpen(false);
+    };
 
     return (
         <>
@@ -16,7 +40,7 @@ const NavBar = () => {
                         src={logo}
                         alt='blueprint logo of the app'
                     /></NavLink>
-
+                    <h1 className='nav__title' >Construction Connect</h1>
                     <div className='nav__log-in' >
                         {loggedIn ? (
                             <>
@@ -38,8 +62,37 @@ const NavBar = () => {
                             </>
                         )}
                     </div>
+                    <div ref={navMenuRef} className={`nav__menu ${isMenuOpen ? 'active' : ''}`}>
+                        {loggedIn ? (
+                            <>
+                                <NavLink to="./post">
+                                    <button className='nav__button' onClick={handleLinkClick}>New Post!</button>
+                                </NavLink>
+                                <button className='nav__button' onClick={() => {
+                                    logOut(); 
+                                    handleLinkClick()}} >
+                                    Logout
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <NavLink to="/login">
+                                    <button className='nav__button' onClick={handleLinkClick}>Log In</button>
+                                </NavLink>
+                                <NavLink to="/signup">
+                                    <button className='nav__button' onClick={handleLinkClick}>Sign Up</button>
+                                </NavLink>
+                            </>
+                        )}
+                    </div>
+
+                    <div className='hamburger' onClick={toggleMenu}>
+                        <span className='bar'></span>
+                        <span className='bar'></span>
+                        <span className='bar'></span>
+                    </div>
                 </nav>
-                <h1 className='nav__title' >Construction Connect</h1>
+                
             </header>
         </>
     )
